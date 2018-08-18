47e52b061240dacda0a26a2c4b03afedbee64da6
Victor Kulitsky <30865977+Pringar@users.noreply.github.com>
2018-08-18 23:05:24 +0300

    Добавил основные исключения

---
+++ src/Core/Vk.CSharp.Sdk/Vk.CSharp.Sdk/Global/Exceptions/InvalidAccessTokenException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Vk.CSharp.Sdk.Global.Exceptions
+{
+    /// <summary>
+    /// Исключение, выбрасываемое при неверном или отсутствующем ключе доступа.
+    /// </summary>
+    public class InvalidAccessTokenException : Exception
+    {
+        public InvalidAccessTokenException() { }
+
+        public InvalidAccessTokenException(string message)
+            : base(message) { }
+
+        public InvalidAccessTokenException(string message, Exception inner)
+            : base(message, inner) { }
+    }
+}
---
+++ src/Core/Vk.CSharp.Sdk/Vk.CSharp.Sdk/Global/Exceptions/VkApiException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Vk.CSharp.Sdk.Global.Exceptions
+{
+    /// <summary>
+    /// Базовое класс для всех исключений, выбрасываемых библиотекой.
+    /// </summary>
+    public class VkApiException : Exception
+    {
+        public VkApiException() { }
+
+        public VkApiException(string message)
+            : base(message) { }
+
+        public VkApiException(string message, Exception inner)
+            : base(message, inner) { }
+    }
+}
